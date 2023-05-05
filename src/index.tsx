b612134a441c9dcf27520f68cd1db4e58e14c1d8
Harry <anuraagvr555@gmail.com>
2023-05-05 23:20:01 +0530

    style: setup some styling variables

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/global.css';
+import './styles/variables.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement).render(
   <StrictMode>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ['./src/**/*.{tsx,jsx}', './src/*.{tsx.jsx}'],
   theme: {
-    extend: {},
+    fontSize: {
+      base: 'clamp(16px, 1.563vw, 20px)',
+    },
+    extend: {
+      backgroundColor: {
+        primary: '#fff',
+        'discord-blue': 'var(--discord-blue)',
+        testimony: '#ffefc5',
+      },
+      fontSize: {
+        navlink: 'clamp(14px, 1.25vw, 18px)',
+      },
+      margin: {
+        'navbar-x': 'var(--navbar-x)',
+        'navbar-y': 'var(--navbar-y)',
+      },
+      padding: {
+        'navbar-x': 'var(--navbar-x)',
+        'navbar-y': 'var(--navbar-y)',
+      },
+    },
   },
   plugins: [],
 };

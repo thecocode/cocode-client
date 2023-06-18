3116f9459d3cd7772552386be0a8da543f5bb1a4
Harry <anuraagvr555@gmail.com>
2023-06-18 21:39:34 +0530

    style: change font size of submit btn for mobile

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -1,27 +1,32 @@
+import { useMediaQuery } from '../../hooks';
+import { ScreenTypes } from '../../types';
 import { BaseButton } from '../Button';
 import { Dropdown } from '../Dropdown';
 import { Input, TextArea } from '../Inputs';
 
 function RightArrowIcon() {
   return (
-    <svg
-      className='mt-1 -mr-3'
-      width='40'
-      height='15'
-      viewBox='0 0 40 15'
-      fill='none'
-      xmlns='http://www.w3.org/2000/svg'
-    >
-      <path
-        d='M0.999939 7.21548L37.5654 7.13593M37.5654 7.13593L31.2899 13.6611M37.5654 7.13593L30.9954 1.5092'
-        stroke='black'
-        strokeWidth='2'
-      />
-    </svg>
+    <span className='block'>
+      <svg
+        className='mt-1 -mr-3 max-w-full'
+        width='40'
+        height='15'
+        viewBox='0 0 40 15'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <path
+          d='M0.999939 7.21548L37.5654 7.13593M37.5654 7.13593L31.2899 13.6611M37.5654 7.13593L30.9954 1.5092'
+          stroke='black'
+          strokeWidth='2'
+        />
+      </svg>
+    </span>
   );
 }
 
 export function ContactForm() {
+  const { screenType } = useMediaQuery();
   return (
     <form>
       <div className='p-[clamp(19px,2.1vw,36px)] rounded bg-primary shadow-contact-form flex flex-col gap-[clamp(16px,1.87vw,32px)]'>
@@ -54,10 +59,11 @@ export function ContactForm() {
           }}
         />
       </div>
-
       <BaseButton
         title='Send Message'
-        extendedClassNames='mt-[clamp(24px,3.3vw,44px)] mx-auto'
+        extendedClassNames={`mt-[clamp(24px,3.3vw,44px)] mx-auto ${
+          screenType === ScreenTypes.MOBILE ? 'text-[13px]' : null
+        }`}
         RightIcon={RightArrowIcon}
       />
     </form>

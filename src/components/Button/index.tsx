1e53c27771516cba91d7912dc2ef53a3cfb0dd97
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2023-07-10 22:11:04 +0530

    style : discord button sizing

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,7 +5,7 @@ export function Button({ link, type }: IButton) {
   return (
     <a href={link} className=''>
       <span
-        className={`flex w-[200px] ml:w-[240px] mx-auto items-center h-[2.5rem] ml:h-[3rem] font-semibold justify-center text-[1.2rem] ml:text-[1.5rem] gap-4 outline outline-2 rounded-[1.5rem] py-3 px-6 ${
+        className={`flex w-[220px] ml:w-[240px] mx-auto items-center h-[2.5rem] ml:h-[3rem] font-semibold justify-center text-[1.2rem] ml:text-[1.5rem] gap-4 outline outline-2 rounded-[1.5rem] py-3 px-6 ${
           type === ButtonTypes.DISCORD ? 'bg-discord-blue text-white' : 'bg-white text-black'
         } `}
       >

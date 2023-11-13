03cd250572f1859418622dd2c2bbc9c069c65bde
iHarryD <anuraagvr555@gmail.com>
2023-11-13 22:23:31 +0530

    fix: prepend envs with REACT_APP_

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -34,7 +34,8 @@ export function ContactForm() {
 
   async function handleSumbit() {
     try {
-      const accessKey = process.env.WEB3FORM_ACCESS_KEY || 'fa502b74-afac-4ee5-9e7b-fe60d40ca888';
+      const accessKey =
+        process.env.REACT_APP_WEB3FORM_ACCESS_KEY || 'fa502b74-afac-4ee5-9e7b-fe60d40ca888';
       if (contactFormRef.current && accessKey) {
         setIsSubmitting(true);
         const formBody = new FormData(contactFormRef.current);
